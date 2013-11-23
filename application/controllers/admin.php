81debd5dfb3f73ba61cb35a778e8e613e1121227
kalasgou <duduleopard@gmail.com>
2013-11-24 00:04:00 +0800

    test from home

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Applicant extends CI_Controller {
+class Admin extends CI_Controller {
 	
 	public function login() {
 		
---
+++ application/controllers/apply.php
@@ -0,0 +1,38 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Apply extends CI_Controller {
+	
+	public function fill_basic_info() {
+		
+	}
+	
+	public function fill_basic_info() {
+		
+	}
+	
+	public function fill_basic_info() {
+		
+	}
+	
+	public function fill_basic_info() {
+		
+	}
+	
+	public function fill_basic_info() {
+		
+	}
+	
+	public function fill_basic_info() {
+		
+	}
+	
+	public function fill_basic_info() {
+		
+	}
+	
+	public function fill_basic_info() {
+		
+	}
+}
+
+/* End of file */

2729c8cc490ca5513160705f8677e9d608033ba8
kalasgou <duduleopard@gmail.com>
2013-11-28 00:57:20 +0800

    test

--- application/views/step_five.php
+++ application/views/step_five.php
@@ -18,11 +18,11 @@
 		</script>
 		<style type="text/css">
 			body {font-family:droid;}
-			#application_form div {padding:2px;}
+			#application_form div {padding:4px 12px; font-size:18px;}
 			#answer {display:inline-block; border-bottom:1px dotted #000; text-align:center;}
 			#option {display:inline-block;}
 			table {border-collapse:collapse; border:1px solid; text-align:center;}
-			th, td {border:1px solid; padding:2px; font-weight: normal;}
+			th, td {border:1px solid; padding:8px; font-weight: normal;}
 		</style>
 	</head>
 	<body>
@@ -36,93 +36,93 @@
 		<div id="application_form">
 			<div>
 				1、Full name 姓名:
-				(English英文) <div id="answer" style="width:200px;">123</div>
-				(Chinese中文) <div id="answer" style="width:200px;">321</div>
+				(English英文) <span id="answer" style="width:200px;">123</span>
+				(Chinese中文) <span id="answer" style="width:200px;">321</span>
 			</div>
 			<div>
 				2、Mr.先生
-				<div id="option">123</div>
-				Mrs.女士 <div id="option">321</div>
-				Miss小姐 <div id="option">321</div>
+				<span id="option"><input type="checkbox" checked="checked"/></span>
+				Mrs.女士 <span id="option"><input type="checkbox"/></span>
+				Miss小姐 <span id="option"><input type="checkbox"/></span>
 			</div>
 			<div>3、Nationality 国籍:
-				<div id="answer" style="width:200px;">123</div>
+				<span id="answer" style="width:200px;">123</span>
 			</div>
 			<div>
 				4、Date of Birth 出生日期:
-				Day 日 <div id="answer" style="width:200px;">123</div>
-				Month 月 <div id="answer" style="width:200px;">321</div>
-				Year 年 <div id="answer" style="width:200px;">321</div>
+				Day 日 <span id="answer" style="width:200px;">123</span>
+				Month 月 <span id="answer" style="width:200px;">321</span>
+				Year 年 <span id="answer" style="width:200px;">321</span>
 			</div>
 			<div>
 				5、Place of birth 出生地点:
-				<div id="answer" style="width:200px;">123</div>
+				<span id="answer" style="width:200px;">123</span>
 			</div>
 			<div>
 				6、Family Situation 婚姻状况:
-				Married已婚 <div id="option">123</div>
-				Single单身 <div id="option">321</div>
-				Widowed丧偶 <div id="option">321</div>
-				Divorced离异 <div id="option">321</div>
+				Married 已婚 <span id="option">123</span>
+				Single 单身 <span id="option">321</span>
+				Widowed 丧偶 <span id="option">321</span>
+				Divorced 离异 <span id="option">321</span>
 			</div>
 			<div>
 				7、Occupation 职业: 
-				<div id="answer" style="width:200px;">123</div>
+				<span id="answer" style="width:200px;">123</span>
 			</div>
 			<div>
 				8、(a) Employer 就业单位:
-				<div id="answer" style="width:200px;">123</div>
-				Tel No.电话: <div id="answer" style="width:200px;">321</div><br>
-				(b) Address 单位地址: <div id="answer" style="width:200px;">321</div>
+				<span id="answer" style="width:200px;">123</span>
+				Tel No.电话: <span id="answer" style="width:200px;">321</span><br>
+				(b) Address 单位地址: <span id="answer" style="width:200px;">321</span>
 			</div>
 			<div>
 				9、Home Address 家庭住址:
-				<div id="answer" style="width:200px;">123</div><br>
-				Tel No.电话: <div id="answer" style="width:200px;">321</div><br>
+				<span id="answer" style="width:200px;">123</span><br>
+				Tel No.电话: <span id="answer" style="width:200px;">321</span><br>
 			</div>
 			<div>
 				10、Passport 护照:<br>
-				(a) Number 护照号 <div id="answer" style="width:200px;">123</div>
-				(b) Place of Issue 发照地 <div id="answer" style="width:200px;">321</div><br>
-				(c) Date of Issue发照日期 <div id="answer" style="width:200px;">321</div>
-				(d) Expiry Date 有效日期至 <div id="answer" style="width:200px;">321</div>
+				(a) Number 护照号 <span id="answer" style="width:200px;">123</span>
+				(b) Place of Issue 发照地 <span id="answer" style="width:200px;">321</span><br>
+				(c) Date of Issue 发照日期 <span id="answer" style="width:200px;">321</span>
+				(d) Expiry Date 有效日期至 <span id="answer" style="width:200px;">321</span>
 			</div>
 			<div>
 				11、Purpose of Visit 访瓦目的:<br>
-				Tourism 旅游 <div id="option">123</div>
-				Visiting Relative 探亲 <div id="option">321</div>
-				Business 商务 <div id="option">321</div>
-				Other 其他 <div id="option">321</div>
+				Tourism 旅游 <span id="option">123</span>
+				Visiting Relative 探亲 <span id="option">321</span>
+				Business 商务 <span id="option">321</span>
+				Other 其他 <span id="option">321</span>
 			</div>
 			<div>
 				12、Address in Vanuatu 在瓦地址:
-				<div id="answer" style="width:200px;">123</div>
+				<span id="answer" style="width:200px;">123</span>
 			</div>
 			<div>
 				13、Details of Family in Vanuatu if visiting relative 如属探亲在瓦亲属概况:<br>
-				Name 姓名: <div id="answer" style="width:100px;">123</div>
-				Add. 地址: <div id="answer" style="width:300px;">123</div>
+				Name 姓名: <span id="answer" style="width:100px;">123</span>
+				Add. 地址: <span id="answer" style="width:300px;">123</span>
 			</div>
 			<div>
 				14、Details of arrival in Vanuatu抵瓦航班号:
-				<div id="answer" style="width:200px;">123</div>
-				日期: <div id="answer" style="width:200px;">123</div>
+				<span id="answer" style="width:200px;">123</span>
+				日期: <span id="answer" style="width:200px;">123</span>
 			</div>
 			<div>
 				15、Details of return ticket 回程航班号:
-				<div id="answer" style="width:200px;">123</div>
-				日期: <div id="answer" style="width:200px;">123</div>
+				<span id="answer" style="width:200px;">123</span>
+				日期: <span id="answer" style="width:200px;">123</span>
 			</div>
 			<div>
 				16、Proposed duration of stay 拟在瓦逗留时间:
-				<div id="answer" style="width:200px;">123</div>
+				<span id="answer" style="width:200px;">123</span>
 			</div>
 			<div>
 				17、Source of financial support in Vanuatu 在瓦费用来源:
-				<div id="answer" style="width:200px;">123</div>
+				<span id="answer" style="width:200px;">123</span>
 			</div>
 			<div>
-				18 Details of family included in passport<br>
+				18、Details of family included in passport<br>
 				护照内偕行儿童详细信息:<br>
 				<table id="children_info">
 					<tr>
@@ -153,22 +153,22 @@
 			</div>
 			<div>
 				19、(a) Been convicted of or have any charges outstanding on a criminal offence in any country<br>
-				是否在任何国家有过犯罪记录: Yes是 <div id="option">321</div>Where哪一国家 <div id="answer" style="width:200px;">123</div>No否 <div id="option">321</div><br>
+				是否在任何国家有过犯罪记录: Yes 是 <span id="option">321</span>Where 哪一国家 <span id="answer" style="width:200px;">123</span>No 否 <span id="option">321</span><br>
 				(b) Been deported or excluded from any country<br>
-				是否有被任何国家驱逐出境的经历: Yes是 <div id="option">321</div>Where哪一国家 <div id="answer" style="width:200px;">123</div>No否 <div id="option">321</div>
+				是否有被任何国家驱逐出境的经历: Yes 是 <span id="option">321</span>Where 哪一国家 <span id="answer" style="width:200px;">123</span>No 否 <span id="option">321</span>
 			</div>
 			<div>
 				20、Details of previous visits? 您曾经到过瓦努阿图吗？<br>
-				Yes有 <div id="option">123</div>
-				No没有 <div id="option">321</div>
+				Yes 有 <span id="option">123</span>
+				No 没有 <span id="option">321</span>
 			</div>
 			<div>
 				21、Have you ever applied for a work, residence or student permit before in Vanuatu?<br>
-				您是否曾经在瓦努阿图申请过工作、居留或学生签证？ Yes是 <div id="option">321</div>When何时 <div id="answer" style="width:200px;">123</div>No否 <div id="option">321</div><br>
+				您是否曾经在瓦努阿图申请过工作、居留或学生签证？ Yes 是 <span id="option">321</span>When 何时 <span id="answer" style="width:200px;">123</span>No 否 <span id="option">321</span><br>
 			</div>
 			<div>
 				22、Have you ever been refused entry to Vanuatu?<br>
-				您曾经被瓦努阿图拒签过吗？ Yes是 <div id="option">321</div>When何时 <div id="answer" style="width:200px;">123</div>No否 <div id="option">321</div><br>
+				您曾经被瓦努阿图拒签过吗？ Yes 是 <span id="option">321</span>When 何时 <span id="answer" style="width:200px;">123</span>No 否 <span id="option">321</span><br>
 			</div>
 		</div>
 	</body>
@@ -189,5 +189,4 @@
 	$dompdf->load_html($HTMLoutput);
 	$dompdf->render();
 	$dompdf->stream('sample.pdf');
-	
 ?>
